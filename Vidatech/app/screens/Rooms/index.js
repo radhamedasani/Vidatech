786c0cbf0e301a575095e378deba1a5e42112d8f
Radha <radha2800@gmail.com>
2019-11-07 08:38:53 +0000

    BurgerMenu Implemenation

--- Vidatech/app/screens/Rooms/index.js
+++ Vidatech/app/screens/Rooms/index.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { Component, useState } from 'react'
 import { View } from 'react-native'
 
 import { baseStyle } from '../../themes/base'
 import { Header } from '../../components/header'
 import { RoomList } from '../../screens/Rooms/RoomList'
+import { BurgerMenu } from '../../components/BurgerMenu'
 
-export const Rooms = () => (
-    <View style={baseStyle.container}>
-        <Header />
-        <RoomList />
-    </View>
-) 
+export const Rooms = () => {
+    const [isBurgermenuVisible, setIsBurgermenuVisible] = useState(false);
+    const showOrHideBurgerMenu = () => {
+        setIsBurgermenuVisible(!isBurgermenuVisible);
+    }
+    return (
+        <View style={baseStyle.container}>
+            <View style={{ flex: 1 }}>
+                <Header onClickBurgerMenu={showOrHideBurgerMenu} />
+                <RoomList />
+            </View>
+            <BurgerMenu isBurgermenuVisible={isBurgermenuVisible}
+                showOrHideBurgerMenu={showOrHideBurgerMenu} />
+        </View>
+    );
+} 
